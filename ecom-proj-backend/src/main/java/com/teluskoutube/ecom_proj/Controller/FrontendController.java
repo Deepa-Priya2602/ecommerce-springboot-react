64e1fb39fb1ed796bbaa3cccda859223c8660f99
Deepa-Priya2602 <deepaoffcamp2602@gmail.com>
2026-04-20 18:11:49 +0530

    Added Dockerfile

---
+++ ecom-proj-backend/src/main/java/com/teluskoutube/ecom_proj/Controller/FrontendController.java
@@ -0,0 +1,13 @@
+package com.teluskoutube.ecom_proj.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+    @Controller
+    public class FrontendController {
+
+        @RequestMapping(value = {"/", "/{path:[^\\.]*}", "/**/{path:[^\\.]*}"})
+        public String forward() {
+            return "forward:/index.html";
+        }
+    }
